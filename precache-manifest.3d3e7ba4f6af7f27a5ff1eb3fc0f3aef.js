de27c78b51ee739eb2e2265d721233b7f5225692
simolee <patelsimolee12@gmail.com>
2020-09-05 19:05:14 -1100

    Updates

---
+++ precache-manifest.3d3e7ba4f6af7f27a5ff1eb3fc0f3aef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2d7b6e1f519361cc353c919a7c6759",
+    "url": "/simolee-patel.github.io/react-weather-app/index.html"
+  },
+  {
+    "revision": "8584c2b6e54946fa5c3b",
+    "url": "/simolee-patel.github.io/react-weather-app/static/css/main.4c299605.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/simolee-patel.github.io/react-weather-app/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simolee-patel.github.io/react-weather-app/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8584c2b6e54946fa5c3b",
+    "url": "/simolee-patel.github.io/react-weather-app/static/js/main.751137f0.chunk.js"
+  },
+  {
+    "revision": "d6798d4f50d035fd8f41",
+    "url": "/simolee-patel.github.io/react-weather-app/static/js/runtime-main.4956b989.js"
+  },
+  {
+    "revision": "0f7a0ef003f270cfc59557cafd5d56af",
+    "url": "/simolee-patel.github.io/react-weather-app/static/media/hot.0f7a0ef0.png"
+  },
+  {
+    "revision": "d6355bcd7b78ca60806339bfc11dddb2",
+    "url": "/simolee-patel.github.io/react-weather-app/static/media/morning.d6355bcd.png"
+  }
+]);
